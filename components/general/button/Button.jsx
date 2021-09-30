399987f5317968b2016f72c1321f37f738890ba5
Matvey Melishev <58311421+melishev@users.noreply.github.com>
2021-09-30 20:27:58 +0300

    change

--- components/general/button/Button.jsx
+++ components/general/button/Button.jsx
@@ -4,7 +4,6 @@ import 'antd/dist/antd.css';
 const ButtonAnt = () => {
   return (
     <>
-      <Button type="primary">Primary Button</Button>
       <Button>Default Button</Button>
       <Button type="dashed">Dashed Button</Button>
       <br />

1072c20088bb9b71bc31f84da86c16a7aa5916cd
Matvey Melishev <58311421+melishev@users.noreply.github.com>
2021-10-06 00:15:18 +0300

    📦 NEW: style and font

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,6 @@
+import './styles.scss'; // Styles for StoryBook
+import '../assets/styles/global.scss'; // Global styles UI library
+
 export const parameters = {
   actions: { argTypesRegex: "^on[a-z].*" },
   controls: {
--- components/general/button/Button.jsx
+++ components/general/button/Button.jsx
@@ -1,5 +1,4 @@
 import './styles.scss';
-import './ve.scss';
 
 const Button = () => (
   <>

16ef4f644afc05bb6eac6fabd9424200e249fcf4
Matvey Melishev <58311421+melishev@users.noreply.github.com>
2021-10-07 00:34:08 +0300

    🐞 fix(Fix bugs): testing action

--- components/general/button/button.jsx
+++ components/general/button/button.jsx
@@ -2,7 +2,7 @@ import './styles.scss';
 
 const Button = () => (
   <>
-    <button type="button">Sign In</button>
+    <button type="button">Sign Out</button>
     <button type="button" className="small">Sign In</button>
     <button type="button" disabled>Sign In</button>
   </>

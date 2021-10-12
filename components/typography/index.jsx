7038e3f59578c21a46862fdc158a48c6fd514880
Matvey Melishev <58311421+melishev@users.noreply.github.com>
2021-10-12 08:15:52 +0300

    🧪 test(typography): test typography component

---
+++ components/typography/index.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable object-shorthand */
+import './styles.scss';
+// import propTypes from 'prop-types';
+
+const Typography = () => (
+  <p>SF Pro Text</p>
+);
+
+// Typography.propTypes = {
+
+// };
+
+// Typography.defaultProps = {
+
+// };
+
+export default Typography;
---
+++ components/typography/index.stories.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+
+import TypographyC from './index';
+
+export default {
+  title: 'Typography',
+  component: TypographyC,
+};
+
+export const Typography = () => <TypographyC />;

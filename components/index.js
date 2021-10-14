cc4cbf73f622471b60cd0e094cbfbdf484ef0497
Matvey Melishev <58311421+melishev@users.noreply.github.com>
2021-10-15 02:30:48 +0300

    🧪 test(components): add test components

--- components/index.js
+++ components/index.js
@@ -1,1 +1,4 @@
+export { default as WookieUI } from './wookieUI';
+
 export { default as Button } from './button';
+export { default as Typography } from './typography';
---
+++ components/wookieUI/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.scss';
+import propTypes from 'prop-types';
+
+const WookieUI = ({ children }) => (
+  <>
+    {children}
+  </>
+);
+
+WookieUI.propTypes = {
+  children: propTypes.node.isRequired,
+};
+
+export default WookieUI;

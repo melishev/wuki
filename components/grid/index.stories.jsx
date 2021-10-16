3a51966d52d1fbfdaacf1c2590feb9b4e0f3f78c
Matvey Melishev <58311421+melishev@users.noreply.github.com>
2021-10-16 20:19:03 +0300

    📦 feat: alpha

---
+++ components/grid/index.stories.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+
+import Grids from './index';
+
+export default {
+  title: 'Grid',
+  component: Grids,
+};
+
+export const Grid = () => <Grids />;

5eea9f3e4d85284f0d948aeb14441e3d0e188f47
Matvey Melishev <58311421+melishev@users.noreply.github.com>
2021-10-28 13:30:10 +0300

    style: 🌈 remove grid from storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -9,4 +9,48 @@ export const parameters = {
     },
     expanded: true,
   },
+  backgrounds: {
+    grid: {
+      disable: true,
+    }
+  },
+  viewport: {
+    viewports: {
+      xs: {
+        name: 'SM - 576px',
+        styles: {
+          width: '576px',
+          height: '100%',
+        },
+      },
+      md: {
+        name: 'MD - 768px',
+        styles: {
+          width: '768px',
+          height: '100%',
+        },
+      },
+      lg: {
+        name: 'LG - 992px',
+        styles: {
+          width: '992px',
+          height: '100%',
+        },
+      },
+      xl: {
+        name: 'XL - 1200px',
+        styles: {
+          width: '1200px',
+          height: '100%',
+        },
+      },
+      xxl: {
+        name: 'XXL - 1400px',
+        styles: {
+          width: '1400px',
+          height: '100%',
+        },
+      },
+    },
+  },
 }

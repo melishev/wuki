e34935f7b0619c6e30ff26eb7ad273c6bd2cf869
Matvey Melishev <matvey@melishev.ru>
2021-11-17 02:08:32 +0300

    perf: ⚡️ rewrote component from scss to jss

--- components/typography/index.jsx
+++ components/typography/index.jsx
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import propTypes from 'prop-types';
-import './index.scss';
+import jss from './styles';
 
-const Text = ({ children, tag: Tag }) => <Tag>{children}</Tag>;
+const Text = ({ children, tag: Tag, textStyle: TextStyle }) => {
+  const styles = jss();
+  const [textStyle, setTextStyle] = useState('');
+
+  useEffect(() => {
+    if (['h1', 'h2', 'h3', 'h4', 'h5', 'h6'].includes(Tag)) {
+      setTextStyle(TextStyle || Tag);
+    } else {
+      setTextStyle(TextStyle);
+    }
+  }, [Tag]);
+
+  return <Tag className={styles[textStyle]}>{children}</Tag>;
+};
 
 Text.propTypes = {
   children: propTypes.node,
   tag: propTypes.string,
+  textStyle: propTypes.string,
 };
 
 Text.defaultProps = {
   children: '',
   tag: 'p',
+  textStyle: '',
 };
 
 export default Text;
--- components/typography/index.stories.jsx
+++ components/typography/index.stories.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable react/jsx-props-no-spreading, react/prop-types */
 import React from 'react';
+import { ThemeProvider } from 'react-jss';
+import theme from '../utils/theme';
 
 import Text from './index';
 
@@ -11,18 +13,24 @@ export default {
   component: Text,
 };
 
-const Information = ({ title, description }) => (
-  <Row>
-    <Col col={24}>
-      <h1>{title}</h1>
-      <p>{description}</p>
-    </Col>
-  </Row>
+const WrapperStoryBook = ({
+  children,
+  title = 'Title',
+  description = 'Description',
+}) => (
+  <ThemeProvider theme={theme}>
+    <Row>
+      <Col col={24}>
+        <h1>{title}</h1>
+        <p>{description}</p>
+      </Col>
+    </Row>
+    {children}
+  </ThemeProvider>
 );
 
-export const Typography = () => (
-  <>
-    <Information title="Title" description="Description" />
+export const Default = () => (
+  <WrapperStoryBook>
     <Row>
       <Col col={24}>
         <Text tag="h1">The quick brown fox jumps over the lazy dog</Text>
@@ -42,6 +50,26 @@ export const Typography = () => (
       <Col col={24}>
         <Text tag="h6">The quick brown fox jumps over the lazy dog</Text>
       </Col>
+      <Col col={24}>
+        <Text tag="p" textStyle="subTitle">
+          The quick brown fox jumps over the lazy dog
+        </Text>
+      </Col>
+      <Col col={24}>
+        <Text tag="p" textStyle="body1">
+          The quick brown fox jumps over the lazy dog
+        </Text>
+      </Col>
+      <Col col={24}>
+        <Text tag="p" textStyle="body2">
+          The quick brown fox jumps over the lazy dog
+        </Text>
+      </Col>
+      <Col col={24}>
+        <Text tag="small" textStyle="small">
+          The quick brown fox jumps over the lazy dog
+        </Text>
+      </Col>
     </Row>
-  </>
+  </WrapperStoryBook>
 );

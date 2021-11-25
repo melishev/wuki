7be14457b35e9f00b0cbb1d2256af5ee173ce8f8
Matvey Melishev <58311421+melishev@users.noreply.github.com>
2021-11-26 00:14:33 +0300

    chore: 🐳 remove comments

--- components/utils/globalStyleSheet.js
+++ components/utils/globalStyleSheet.js
@@ -1,13 +1,5 @@
 import { createUseStyles } from 'react-jss';
 
-// import '@fontsource/manrope/200.css';
-// import '@fontsource/manrope/300.css';
-// import '@fontsource/manrope/400.css';
-// import '@fontsource/manrope/500.css';
-// import '@fontsource/manrope/600.css';
-// import '@fontsource/manrope/700.css';
-// import '@fontsource/manrope/800.css';
-
 export default function style(theme) {
   return createUseStyles({
     '@global': {

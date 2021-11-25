737203af6649308a0bbe95b93142d53cc50aa927
Matvey Melishev <58311421+melishev@users.noreply.github.com>
2021-11-26 00:14:58 +0300

    fix: 🐛 prettier error

--- components/utils/normalize.js
+++ components/utils/normalize.js
@@ -39,10 +39,12 @@ const converted = createUseStyles({
     'button, [type="button"], [type="reset"], [type="submit"]': {
       WebkitAppearance: 'button',
     },
+    // prettier-ignore
     'button::-moz-focus-inner,\n[type="button"]::-moz-focus-inner, [type="reset"]::-moz-focus-inner, [type="submit"]::-moz-focus-inner': {
       borderStyle: 'none',
       padding: '0',
     },
+    // prettier-ignore
     'button:-moz-focusring, [type="button"]:-moz-focusring, [type="reset"]:-moz-focusring, [type="submit"]:-moz-focusring': {
       outline: '1px dotted ButtonText',
     },
@@ -61,6 +63,7 @@ const converted = createUseStyles({
       boxSizing: 'border-box',
       padding: '0',
     },
+    // prettier-ignore
     '[type="number"]::-webkit-inner-spin-button, [type="number"]::-webkit-outer-spin-button': {
       height: 'auto',
     },

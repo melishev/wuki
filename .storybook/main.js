6ddd3813059549138676eb2d3caaec02c7509f3a
Matvey Melishev <58311421+melishev@users.noreply.github.com>
2021-11-26 00:36:55 +0300

    build: 🚀 add new scripts, global styles StoryBook, addon-a11y, basic settings (#52)
    * chore: 🐳 add new grid plugin for StoryBook, add link for StoryBook project
    
    * build: 🚀 add basic customization for StoryBook
    
    * docs: 📝 add test documentation
    
    * feat: 📦 add lang toggle
    
    * build: 🚀 add new scripts, global styles StoryBook, addon-a11y
    
    * refactor: ♻️ export for Node.js
    
    * style: 🌈 corrected StoryBook settings
    
    * docs: 📝 prepared test documentation format

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const theme = require('../components/utils/theme');
+
 module.exports = {
   stories: [
     '../components/**/*.stories.@(js|jsx|mdx)'
@@ -6,5 +8,19 @@ module.exports = {
     '@storybook/addon-docs',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-  ]
+    'storybook-addon-grid/preset',
+    'storybook-addon-grid/chromatic',
+    '@storybook/addon-a11y',
+  ],
+  previewHead: (head) => (`
+    ${head}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap">
+    <style>
+      h1, h2, h3, h4, h5, h6, p {
+        font-family: ${theme.type.family} !important;
+      }
+    </style>
+  `)
 }

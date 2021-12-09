bf534ae94134c7672f5e87e345b0883f04cebb37
matvey.talinskiy <mpt@1gx.org>
2021-12-09 12:28:53 +0300

    test: 🧪 fix eslint error

--- components/grid/index.stories.jsx
+++ components/grid/index.stories.jsx
@@ -21,26 +21,24 @@ const styleCol = {
   border: '1px solid black',
 };
 
-export function Default() {
-  return (
-    <Grid container>
-      {[...Array(24)].map((item, index) => (
-        <Grid key={item} style={styleCol}>
-          <Text variant="body2">{index + 1}</Text>
-        </Grid>
-      ))}
-      <Grid col={8} style={styleCol}>
-        <Text variant="body2">8</Text>
+export const Default = () => (
+  <Grid container>
+    {[...Array(24)].map((item, index) => (
+      <Grid key={item} style={styleCol}>
+        <Text variant="body2">{index + 1}</Text>
       </Grid>
-      <Grid col={12} style={styleCol}>
-        <Text variant="body2">12</Text>
-      </Grid>
-      <Grid col={4} style={styleCol}>
-        <Text variant="body2">4</Text>
-      </Grid>
+    ))}
+    <Grid col={8} style={styleCol}>
+      <Text variant="body2">8</Text>
     </Grid>
-  );
-}
+    <Grid col={12} style={styleCol}>
+      <Text variant="body2">12</Text>
+    </Grid>
+    <Grid col={4} style={styleCol}>
+      <Text variant="body2">4</Text>
+    </Grid>
+  </Grid>
+);
 
 export const Offset = () => (
   <Grid container>

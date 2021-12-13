d391284f64ec4290c7a93c17e35d6385ccee35fe
Matvey Melishev <58311421+melishev@users.noreply.github.com>
2021-12-14 00:16:39 +0300

    test: 🚨 add tests for Provider (#72)
    * docs(storybook): 📚️ add stories
    
    * test: 🚨 try write tests
    
    * test: 🚨 add tests for component

--- components/text/index.test.jsx
+++ components/text/index.test.jsx
@@ -3,11 +3,30 @@ import { render, screen } from '../utils/setupTests';
 import { Text } from '../index';
 
 describe('Text', () => {
-  it('renders welcome message', () => {
+  it('`Hello, world!` message', () => {
     render(
       <Text>Hello, world!</Text>,
     );
-    screen.debug();
-    expect(screen.getByText('Hello, world!')).toBeInTheDocument();
+    expect(screen.getByText('Hello, world!')).toBeInTheDocument(); // Исправить на нахождение текста
   });
+  it('with a className equal to the Tag', () => {
+    render(
+      <Text tag="h1">Heading 1</Text>,
+    );
+    expect(screen.getByText('Heading 1')).toHaveClass('wk-h1');
+  });
+  it('with a className equal to the variant', () => {
+    render(
+      <Text variant="body1">Heading 1</Text>,
+    );
+    expect(screen.getByText('Heading 1')).toHaveClass('wk-body1');
+  });
+  // it('with a className equal to the style', () => {
+  //   render(
+  //     <Text style={{ color: 'red' }}>Inline styles</Text>,
+  //   );
+  //   screen.debug();
+  //   expect(screen.getByText('Inline styles')).toHaveClass(/wk-s/i);
+  // });
+  // /wk-s/i - тестировать встроенные в класс строковые стили
 });

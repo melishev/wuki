f76037de2779de9057271df0be5c26d15b137c6c
Matvey Melishev <58311421+melishev@users.noreply.github.com>
2021-12-16 00:42:52 +0300

    feat(docs): add IBM Plex mono font to StoryBook

--- .storybook/main.js
+++ .storybook/main.js
@@ -15,7 +15,7 @@ module.exports = {
     ${head}
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&family=Manrope:wght@200;300;400;500;600;700;800&display=swap">
     <style>
       body {
         padding-left: 0 !important;

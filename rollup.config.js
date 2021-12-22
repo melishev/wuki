599c5b59f05596417b3d09c5fd8840425e52dd2e
Matvey Melishev <58311421+melishev@users.noreply.github.com>
2021-12-22 04:29:14 +0300

    perf(rollup): 💥 ⚡️ update build config
    update the build configuration, the bundle size is smaller
    
    BREAKING CHANGE: now only a single index.js file is available, which contains all the components

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import copy from 'rollup-plugin-copy';
+// import copy from 'rollup-plugin-copy';
 import { babel } from '@rollup/plugin-babel';
 import cleaner from 'rollup-plugin-cleaner';
 import analyze from 'rollup-plugin-analyzer';
@@ -7,16 +7,25 @@ import commonjs from '@rollup/plugin-commonjs';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
-import multiInput from 'rollup-plugin-multi-input';
+// import multiInput from 'rollup-plugin-multi-input';
 
 const extensions = ['.js', '.jsx'];
 
 const plugins = [
+  cleaner({ targets: ['./dist/'] }),
   commonjs(),
   babel({
-    exclude: 'node_modules/**',
-    extensions,
-    presets: ['@babel/preset-env', '@babel/preset-react'],
+    presets: [
+      '@babel/preset-env',
+      '@babel/preset-react',
+    ],
+    plugins: [
+      ['babel-plugin-transform-react-remove-prop-types',
+        {
+          removeImport: true,
+        },
+      ],
+    ],
   }),
   nodeResolve({
     extensions,
@@ -29,35 +38,81 @@ const plugins = [
 ];
 
 export default [
+  // {
+  //   input: ['components/**/index.jsx'],
+  //   output: [
+  //     {
+  //       format: 'cjs',
+  //       exports: 'named',
+  //       dir: 'dist/cjs',
+  //     },
+  //   ],
+  //   plugins: [
+  //     ...plugins,
+  //     babel({
+  //       exclude: 'node_modules/**',
+  //       extensions,
+  //       presets: [
+  //         '@babel/preset-env',
+  //         '@babel/preset-react',
+  //       ],
+  //     }),
+  //     multiInput({ relative: 'components/' }),
+  //     cleaner({ targets: ['./dist/'] }),
+  //   ],
+  // },
   {
-    input: ['components/**/index.jsx'],
+    input: ['components/index.js'],
     output: [
       {
         format: 'cjs',
         exports: 'named',
         dir: 'dist/cjs',
       },
-    ],
-    plugins: [
-      ...plugins,
-      multiInput({ relative: 'components/' }),
-      cleaner({ targets: ['./dist/'] }),
-      copy({
-        targets: [{ src: 'components/index.js', dest: 'dist/cjs' }],
-        verbose: true,
-        copyOnce: true,
-      }),
-    ],
-  },
-  {
-    input: ['components/index.js'],
-    output: [
       {
         format: 'esm',
         exports: 'named',
         dir: 'dist/esm',
       },
     ],
-    plugins,
+    plugins: [
+      ...plugins,
+    ],
   },
+  // {
+  //   input: ['components/index.js'],
+  //   output: [
+  //     {
+  //       format: 'esm',
+  //       exports: 'named',
+  //       dir: 'dist/esm',
+  //     },
+  //   ],
+  //   plugins: [
+  //     ...plugins,
+  //     babel({
+  //       babelHelpers: 'runtime',
+  //       exclude: 'node_modules/**',
+  //       extensions,
+  //       presets: [
+  //         ['@babel/preset-env',
+  //           {
+  //             targets: {
+  //               esmodules: true,
+  //             },
+  //           },
+  //         ],
+  //         '@babel/preset-react',
+  //       ],
+  //       plugins: [
+  //         ['@babel/plugin-transform-runtime',
+  //           {
+  //             helpers: true,
+  //           },
+  //         ],
+  //         'babel-plugin-transform-react-remove-prop-types',
+  //       ],
+  //     }),
+  //   ],
+  // },
 ];

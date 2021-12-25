6fe54c7f33924345badbdf133a201c58a393f6a2
Matvey Melishev <58311421+melishev@users.noreply.github.com>
2021-12-26 01:52:16 +0300

    feat(theme): ✨ add color system

---
+++ components/utils/theme/type.js
@@ -0,0 +1,149 @@
+module.exports = {
+  base: 16,
+  family:
+      'Manrope, -apple-system, BlinkMacSystemFont, "Segoe UI", "Apple Color Emoji", "Segoe UI Emoji", sans-serif',
+  variants: {
+    h1: {
+      size: '2.566rem',
+      weight: 'bold',
+      height: 1.18,
+      breakPoints: {
+        xs: {
+          size: '1.764rem',
+        },
+        sm: {
+          size: '1.924rem',
+        },
+        md: {
+          size: '2.085rem',
+        },
+        lg: {
+          size: '2.245rem',
+        },
+        xl: {
+          size: '2.406rem',
+        },
+        xxl: {
+          size: '2.566rem',
+        },
+      },
+    },
+    h2: {
+      size: '2.281rem',
+      weight: 'bold',
+      height: 1.2,
+      breakPoints: {
+        xl: {
+          size: '2.138rem',
+        },
+        xxl: {
+          size: '2.281rem',
+        },
+      },
+    },
+    h3: {
+      size: '2.027rem',
+      weight: 'bold',
+      height: 1.24,
+      breakPoints: {
+        xl: {
+          size: '1.901rem',
+        },
+        xxl: {
+          size: '2.027rem',
+        },
+      },
+    },
+    h4: {
+      size: '1.802rem',
+      weight: 'bold',
+      height: 1.26,
+      breakPoints: {
+        xl: {
+          size: '1.689rem',
+        },
+        xxl: {
+          size: '1.802rem',
+        },
+      },
+    },
+    h5: {
+      size: '1.602rem',
+      weight: 'bold',
+      height: 1.24,
+      breakPoints: {
+        xl: {
+          size: '1.502rem',
+        },
+        xxl: {
+          size: '1.602rem',
+        },
+      },
+    },
+    h6: {
+      size: '1.424rem',
+      weight: 'bold',
+      height: 1.22,
+      breakPoints: {
+        xl: {
+          size: '1.335rem',
+        },
+        xxl: {
+          size: '1.424rem',
+        },
+      },
+    },
+    subTitle: {
+      size: '1.266rem',
+      weight: 600,
+      height: 1.4,
+      breakPoints: {
+        xl: {
+          size: '1.186rem',
+        },
+        xxl: {
+          size: '1.266rem',
+        },
+      },
+    },
+    body1: {
+      size: '1.125rem',
+      weight: 500,
+      height: 1.54,
+      breakPoints: {
+        xl: {
+          size: '1.055rem',
+        },
+        xxl: {
+          size: '1.125rem',
+        },
+      },
+    },
+    body2: {
+      size: '1rem',
+      weight: 400,
+      height: 1.48,
+      breakPoints: {
+        xl: {
+          size: '0.938rem',
+        },
+        xxl: {
+          size: '1rem',
+        },
+      },
+    },
+    small: {
+      size: '0.79rem',
+      weight: 500,
+      height: 1.24,
+      breakPoints: {
+        xl: {
+          size: '0.741rem',
+        },
+        xxl: {
+          size: '0.79rem',
+        },
+      },
+    },
+  },
+};

953ee5803e3c1dc99f5983c39b4037a3d57e38da
Matvey Melishev <58311421+melishev@users.noreply.github.com>
2021-12-29 01:27:10 +0300

    feat(components): ✨ add new color to theme

--- components/code/styles.js
+++ components/code/styles.js
@@ -1,68 +1,62 @@
 import { createUseStyles } from 'react-jss';
 import { createGenerateId } from '../utils/helpers';
 
-const colors = {
-  gray1: '#E5E5E5',
-  gray2: '#6D6D6D',
-  gray3: '#1D1D1F',
-  orange1: '#FFB45B',
-};
-
-const globalCSS = {
-  background: 'white',
-  border: `1px solid ${colors.gray1}`,
-  font: '400 .9rem IBM Plex Mono',
-  color: colors.gray3,
-};
-
-const useStyles = createUseStyles({
-  codeInline: {
-    ...globalCSS,
-    display: 'inline-block',
-    padding: 'calc(0.25rem - 1px) calc(.75rem - 1px)',
-    borderRadius: 6,
-    cursor: 'copy',
-    '&:hover': {
-      borderColor: colors.gray2,
-    },
-    '&.active': {
-      borderColor: colors.orange1,
-    },
-  },
-  code: {
-    ...globalCSS,
-    padding: 'calc(1rem - 1px)',
-    borderRadius: 10,
-    overflow: 'auto',
-    '&.active': {
-      '& button': {
-        borderColor: colors.orange1,
-        stroke: colors.orange1,
-      },
-    },
-    '& button': {
-      display: 'flex',
-      background: 'white',
-      border: `1px solid ${colors.gray1}`,
+const useStyles = createUseStyles(({ colors }) => {
+  const globalCSS = {
+    background: 'white',
+    border: `1px solid ${colors.grey[200]}`,
+    font: '400 .9rem IBM Plex Mono',
+    color: colors.grey[900],
+  };
+  return {
+    codeInline: {
+      ...globalCSS,
+      display: 'inline-block',
+      padding: 'calc(0.25rem - 1px) calc(.75rem - 1px)',
       borderRadius: 6,
-      padding: 'calc(1rem - 10px - 1px)',
-      position: 'sticky',
-      float: 'right',
-      left: 'calc(100% - 2rem)',
-      stroke: colors.gray1,
-      cursor: 'pointer',
-      '& svg': {
-        stroke: 'inherit',
-      },
+      cursor: 'copy',
       '&:hover': {
-        borderColor: colors.gray2,
-        stroke: colors.gray2,
+        borderColor: colors.grey[500],
+      },
+      '&.active': {
+        borderColor: colors.orange[500],
       },
     },
-    '& code': {
-      marginRight: '3rem',
+    code: {
+      ...globalCSS,
+      padding: 'calc(1rem - 1px)',
+      borderRadius: 10,
+      overflow: 'auto',
+      '&.active': {
+        '& button': {
+          borderColor: colors.orange[500],
+          stroke: colors.orange[500],
+        },
+      },
+      '& button': {
+        display: 'flex',
+        background: 'white',
+        border: `1px solid ${colors.grey[200]}`,
+        borderRadius: 6,
+        padding: 'calc(1rem - 10px - 1px)',
+        position: 'sticky',
+        float: 'right',
+        left: 'calc(100% - 2rem)',
+        stroke: colors.grey[200],
+        cursor: 'pointer',
+        '& svg': {
+          stroke: 'inherit',
+        },
+        '&:hover': {
+          borderColor: colors.grey[500],
+          stroke: colors.grey[500],
+        },
+      },
+      '& code': {
+        marginRight: '3rem',
+      },
     },
-  },
+  };
 },
 { generateId: createGenerateId() });
 

bdb68729607040ab205503c271090fe35999ac07
Matvey Melishev <58311421+melishev@users.noreply.github.com>
2022-01-09 23:37:10 +0300

    refactor(storybook): ♻️ create a template for unified component documentation

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { themes } from '@storybook/theming';
 
 import { grid } from '../components/utils/theme';
-import { WukiProvider, Text, Code } from '../components';
+import { WukiProvider, Text } from '../components';
 
-import { DocsContainer, DocsStory, ArgsTable } from '@storybook/addon-docs';
+import { DocsContainer } from '@storybook/addon-docs';
 
 import { I18nextProvider, useTranslation } from 'react-i18next';
-import i18nConfig from './helpers/i18next.js'
+import i18nConfig from './helpers/i18next.js';
+
+import DocsWrapper from './components/docsWrapper';
 
 export const decorators = [
   (Story, { args }) => (
@@ -24,31 +26,6 @@ export const parameters = {
     container: ({ children, context }) => {
       const { t, i18n } = useTranslation()
 
-      const Universal = () => {
-        const { name } = context.component
-
-        return (
-          <>
-            <Text tag="h1" variant="h2">{name}</Text>
-            <Text variant="body2" style={{margin: '16px 0'}}>{useTranslation(name.toLowerCase()).t('description')}</Text>
-            <Code code={`import { ${name} } from 'wuki'`} />
-            {children}
-            <Text tag="h2" variant="h5">{useTranslation(name.toLowerCase()).t('examples.title')}</Text>
-            {context.componentStories().map((story) => (
-              <div key={story.id}>
-                <h3>{useTranslation(name.toLowerCase()).t(`examples.${story.name.toLowerCase()}.title`)}</h3>
-                <p>{useTranslation(name.toLowerCase()).t(`examples.${story.name.toLowerCase()}.description`)}</p>
-                <DocsStory id={story.id} />
-              </div>
-            ))}
-            <Text tag="h2" variant="h5">Component API (props)</Text>
-            <p>Благодаря простому API(props) вы можете настроить компонент под ваши нужды. Чаще всего в этом не будет нужды, но для лучшего эффекта они и существуют</p>
-            <p>Divide the table into two parts, the first will contain the props of the component itself, the second will contain global</p>
-            <ArgsTable />
-          </>
-        )
-      }
-
       return (
       <I18nextProvider i18n={i18nConfig}>
         <WukiProvider cssBaseLine>
@@ -56,7 +33,7 @@ export const parameters = {
             <button><Text variant='body2' onClick={() => i18n.changeLanguage('ru')}>Russia 🇷🇺</Text></button>
             <button><Text variant='body2' onClick={() => i18n.changeLanguage('en')}>English 🇺🇸</Text></button>
             {context.component
-              ? <Universal />
+              ? <DocsWrapper context={context}>{children}</DocsWrapper>
               : children
             }
           </DocsContainer>

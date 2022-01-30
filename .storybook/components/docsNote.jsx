ba375910330444d9c5742ab5dcb0c56c4aff44d7
Matvey Melishev <58311421+melishev@users.noreply.github.com>
2022-01-31 02:06:57 +0300

    docs(storybook): 📝 add new component for documentation, edit installation documentation

---
+++ .storybook/components/docsNote.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text } from '../../components'
+
+function docsNote({children, status}) {
+  return (
+    <div style={{ padding: '16px 20px', background: '#E9E9E9', borderRadius: 4}}>
+      <Text variant="body1" style={{ margin: '0 0 4px'}}>🚸 {status}</Text>
+      <Text variant="body2" style={{ margin: 0}}>{children}</Text>
+    </div>
+  )
+}
+
+export default docsNote;

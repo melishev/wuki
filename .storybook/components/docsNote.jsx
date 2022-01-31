617ade72ba65a343b6c1c214d343e8d70fd0de11
Matvey Melishev <58311421+melishev@users.noreply.github.com>
2022-01-31 16:23:01 +0300

    docs(storybook): 📝 add padding

--- .storybook/components/docsNote.jsx
+++ .storybook/components/docsNote.jsx
@@ -3,7 +3,7 @@ import { Text } from '../../components'
 
 function docsNote({children, status}) {
   return (
-    <div style={{ padding: '16px 20px', background: '#E9E9E9', borderRadius: 4}}>
+    <div style={{ padding: '16px 20px', background: '#E9E9E9', borderRadius: 10}}>
       <Text variant="body1" style={{ margin: '0 0 4px'}}>🚸 {status}</Text>
       <Text variant="body2" style={{ margin: 0}}>{children}</Text>
     </div>

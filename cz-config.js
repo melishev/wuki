52f50a17c5bc21562843e7e7067b3f9937ec3465
Matvey Melishev <58311421+melishev@users.noreply.github.com>
2022-02-02 00:24:23 +0300

    chore: 🔧 change the appearance of the cz console

--- cz-config.js
+++ cz-config.js
@@ -4,43 +4,43 @@ module.exports = {
   types: [
     {
       value: 'feat',
-      name: `feat: ${devmoji[0].graphic}     A new feature`,
+      name: `${devmoji[0].graphic} feat:      A new feature`,
     },
     {
       value: 'fix',
-      name: `fix: ${devmoji[1].graphic}      A bug fix`,
+      name: `${devmoji[1].graphic} fix:       A bug fix`,
     },
     {
       value: 'docs',
-      name: `docs: ${devmoji[2].graphic}     Documentation only changes`,
+      name: `${devmoji[2].graphic} docs:      Documentation only changes`,
     },
     {
       value: 'refactor',
-      name: `refactor: ${devmoji[3].graphic}  A code change that neither fixes a bug nor adds a feature`,
+      name: `${devmoji[3].graphic}  refactor:  A code change that neither fixes a bug nor adds a feature`,
     },
     {
       value: 'perf',
-      name: `perf: ${devmoji[4].graphic}     A code change that improves performance`,
+      name: `${devmoji[4].graphic} perf:      A code change that improves performance`,
     },
     {
       value: 'test',
-      name: `test: ${devmoji[5].graphic}     Adding missing tests or correcting existing tests`,
+      name: `${devmoji[5].graphic} test:      Adding missing tests or correcting existing tests`,
     },
     {
       value: 'chore',
-      name: `chore: ${devmoji[6].graphic}    Minor work (ex: updated dependencies)`,
+      name: `${devmoji[6].graphic} chore:     Minor work (ex: updated dependencies)`,
     },
     {
       value: 'build',
-      name: `build: ${devmoji[7].graphic}    Changes that affect the build system (ex: rollup, npm)`,
+      name: `${devmoji[7].graphic} build:     Changes that affect the build system (ex: rollup, npm)`,
     },
     {
       value: 'ci',
-      name: `ci: ${devmoji[8].graphic}       Changes to our CI configuration files and scripts\n               (ex: Github Actions)`,
+      name: `${devmoji[8].graphic} ci:        Changes to our CI configuration files and scripts\n                (ex: Github Actions)`,
     },
     {
-      value: 'WIP',
-      name: `WIP: ${devmoji[9].graphic}      Work in progress.`,
+      value: 'wip',
+      name: `${devmoji[9].graphic} wip:       Work in progress.`,
     },
   ],
   scopes: [

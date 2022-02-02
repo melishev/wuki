be5c36ecd1a9b32cb87acb540f1c1acaf36a0a67
Matvey Melishev <58311421+melishev@users.noreply.github.com>
2022-02-02 22:09:50 +0300

    docs(storybook): 📝 remove scrollbar

--- .storybook/main.js
+++ .storybook/main.js
@@ -26,6 +26,12 @@ module.exports = {
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&family=Manrope:wght@200;300;400;500;600;700;800&display=swap">
     <style>
+      ::-webkit-scrollbar {
+        width: 0;
+      }
+      html {
+        scrollbar-width: none;
+      }
       body {
         padding-left: 0 !important;
         padding-right: 0 !important;

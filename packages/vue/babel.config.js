87ea25de58b8a29af8926c82a6e24e5760b21bef
Matvey Melishev <58311421+melishev@users.noreply.github.com>
2022-03-25 15:38:55 +0400

    fix

--- packages/vue/babel.config.js
+++ packages/vue/babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['@babel/preset-env'],
+  plugins: [
+    '@vue/babel-plugin-jsx',
+    [
+      'search-and-replace',
+      {
+        rules: [
+          {
+            search: /_nameFramework_/,
+            replace: 'vue',
+          },
+        ],
+      },
+    ],
+  ],
 };

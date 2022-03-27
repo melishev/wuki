4c6779f70069bd154511a8ac6e8b80fe4de96a93
Matvey Melishev <58311421+melishev@users.noreply.github.com>
2022-03-27 15:02:31 +0400

    fix

--- apps/react/src/main.jsx
+++ apps/react/src/main.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
-import {render} from 'react-dom'
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { I18nextProvider } from 'react-i18next';
+import { WukiProvider } from '@wuki-ui/react';
+import App from './app';
 
-import App from './App'
+import './main.scss';
 
-render(<App />,document.getElementById('root'))
+import i18nConfig from './i18n';
+
+render(
+  <BrowserRouter>
+    <I18nextProvider i18n={i18nConfig}>
+      <WukiProvider cssBaseLine>
+        <App />
+      </WukiProvider>
+    </I18nextProvider>
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
---
+++ apps/react/src/pages/index.js
@@ -0,0 +1,3 @@
+export { default as Start } from './start';
+export { default as Installation } from './installation';
+export { default as Icons } from './icons';
